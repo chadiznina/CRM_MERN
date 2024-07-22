ade410cc3e67eb8585c19e4a807e1ad38aa4d950
chadiznina <chadi.znina@esprit.tn>
2024-07-22 17:11:43 +0100

    fix tasks

--- routes/task.js
+++ routes/task.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middleware/auth');
 const { createTask, getTask, getTasks, updateTask, deleteTask } = require('../controllers/task');
 
 router.route("/create").post(authMiddleware, createTask);
-router.route("/tasks").get(authMiddleware, getTasks);
+router.route("/").get(authMiddleware, getTasks);
 router.route("/task/:id").get(authMiddleware, getTask);
 router.route("/task/:id").put(authMiddleware, updateTask);
 router.route("/task/:id").delete(authMiddleware, deleteTask);
